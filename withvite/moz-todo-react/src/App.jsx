89e0db81270f518c535a07ddd479654909459c0c
SethMegbetor <sethmegbetor@gmail.com>
2024-03-07 17:35:12 +0000

    start the MDN docs. Ended on the map rendering

--- withvite/moz-todo-react/src/App.jsx
+++ withvite/moz-todo-react/src/App.jsx
@@ -1,18 +1,60 @@
-import "./App.css";
+import Todo from "./components/Todo";
 
-const course = "React";
 function App(props) {
-  console.log(props);
+  const taskList = props.tasks?.map((task) => (
+    <Todo name={task.name} id={task.id} completed={task.completed} />
+  ));
+
   return (
-    <>
-      <header>
-        <h1>Leo, {props.greeting}</h1>
-        <h1>Alidu, {props.service}</h1>
-        <button type="button" className="primay">
-          Click Me
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            What needs to be done?
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
+          Add
         </button>
-      </header>
-    </>
+      </form>
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {/* <Todo name="Eat" id="todo-0" completed />
+        <Todo name="Sleep" id="todo-1" />
+        <Todo name="Repeat" id="todo-2" /> */}
+        {taskList}
+      </ul>
+    </div>
   );
 }
+
 export default App;
