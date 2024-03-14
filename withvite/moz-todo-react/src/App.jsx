1172b002b5ab7403d3180e07a820c470127b09cd
SethMegbetor <sethmegbetor@gmail.com>
2024-03-14 12:12:25 +0000

    end of class on Thursday

--- withvite/moz-todo-react/src/App.jsx
+++ withvite/moz-todo-react/src/App.jsx
@@ -5,8 +5,18 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
+// filter map
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
 
   function addTask(name) {
     // alert(name);
@@ -18,20 +28,48 @@ function App(props) {
     // console.log(tasks[0]);
     const updateTasks = tasks.map((task) => {
       if (id === task.id) {
-        return {...task, completed: !task.completed}
+        return { ...task, completed: !task.completed };
       }
       return task;
-    })
-    setTasks(updateTasks)
+    });
+    setTasks(updateTasks);
   }
 
-  const taskList = tasks?.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
 
@@ -43,9 +81,10 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
+        {/* <FilterButton />
         <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        <FilterButton /> */}
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
