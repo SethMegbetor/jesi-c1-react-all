a2109048d47869d373d415370ea311a05b84806c
Seth Megbetor <sethmegbetor@gmail.com>
2024-03-19 20:30:18 +0000

    added a basic react project

---
+++ basicReactApp/src/components/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Plate from "../img/Plate.jpg";
+import "../css/main.css"
+
+export default function Welcome() {
+  return (
+    <div className="wrapper">
+      <h1>Welcome to our app</h1>
+      <h2>I am going to be the best react dev</h2>
+      <img src={Plate} alt="My food image" width={400} height={400} />
+    </div>
+  );
+}
