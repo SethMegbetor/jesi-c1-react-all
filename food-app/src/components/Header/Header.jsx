5583212264a59443b204dd8726760f058d8db874
Seth Megbetor <sethmegbetor@gmail.com>
2024-03-27 12:11:11 +0000

    done with class on Wednesday

--- food-app/src/components/Header/Header.jsx
+++ food-app/src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
-import "./Header.css"
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header">Header</div>
-  )
-}
-export default Header
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order Your Favourite Food Here</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
+          excepturi ipsam nihil dolores praesentium tenetur ea vel vero saepe
+          minus.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+export default Header;
--- food-app/src/pages/Home/Home.jsx
+++ food-app/src/pages/Home/Home.jsx
@@ -1,5 +1,12 @@
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import Header from "../../components/Header/Header";
 import "./Home.css";
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Header />
+      <ExploreMenu/>
+    </div>
+  );
 };
 export default Home;
